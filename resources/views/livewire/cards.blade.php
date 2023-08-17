8a23cd868256f2d76eef255b8704f42d839d9836
Rocco Clemente <roccoclemente224@gmail.com>
2023-08-17 19:56:57 +0200

    nono

--- resources/views/livewire/cards.blade.php
+++ resources/views/livewire/cards.blade.php
@@ -2,7 +2,7 @@
     <div class="row py-5">
         <div>
             <form wire:submit.prevent="cardByGenre" class="category-form col-10 offset-1 mb-5">
-            <div class="w-100 d-flex justify-center align-items-center">
+                <div class="w-100 d-flex justify-center align-items-center">
                     <h3>Una categoria in particolare?</h3>
                 </div>
                 <div class="text-center w-25">
@@ -20,7 +20,7 @@
         </div>
 
         @forelse($announcements as $announcement)
-                <div class="col-12 col-md-4 col-lg-3 my-2 p-3  overflow-hidden">
+        <div class="col-12 col-md-4 col-lg-3 my-2 p-3  overflow-hidden">
             <a href="{{route('announcement', $announcement)}}" class="card-link" target="_blank">
                 <div class="custom-card mx-auto">
                     <div class="custom-img">
@@ -47,7 +47,11 @@
             </a>
         </div>
         @empty
-        <H3>ciao</H3>
+        <div class="col-12 mx-auto text-center search-msg">
+            <p>Non ci sono annunci per questa ricerca! <i class="bi bi-emoji-frown"></i></p>
+            <p>Vuoi publicarne uno tuo? </p>
+            <a href="{{route('announcements.create')}}" class="btn-home">Pubblica</a>
+        </div>
         @endforelse
     </div>
 </div>
