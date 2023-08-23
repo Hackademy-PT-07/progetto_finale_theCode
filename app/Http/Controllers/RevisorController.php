4096d3569d3284f61f166e16571adc6eff82eb6e
Rocco Clemente <roccoclemente224@gmail.com>
2023-08-23 21:42:13 +0200

    completamento pagina revisore

--- app/Http/Controllers/RevisorController.php
+++ app/Http/Controllers/RevisorController.php
@@ -17,13 +17,15 @@ class RevisorController extends Controller
         return view('revisor.index');
     }
 
-    public function becomeRevisor(){
+    public function becomeRevisor()
+    {
         Mail::to('admin@presto.it')->send(new BecomeRevisor(Auth::user()));
         return redirect()->back()->with('message', 'Complimenti, hai chiesto di diventare revisore');
     }
 
-    public function makeRevisor(User  $user){
-        Artisan::call('presto:make-user-revisor', ['email' =>$user->email]);
+    public function makeRevisor(User $user)
+    {
+        Artisan::call('presto:make-user-revisor', ['email' => $user->email]);
         return redirect('/')->with('seccess', "L'utente è diventato revisore!");
     }
 }
