fa1d25f327844972b6b9205f46241b132424997a
giovanni de cola <giovanni_decola@yahoo.com>
2023-08-30 21:32:39 +0200

    css movements

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,41 +1,37 @@
-import './bootstrap';
-import 'bootstrap';
+import "./bootstrap";
+import "bootstrap";
 
-const hamburger = document.getElementById('hamburger')
-const dropList = document.getElementById('dropList')
+const hamburger = document.getElementById("hamburger");
+const dropList = document.getElementById("dropList");
 
 // gestione menù
-hamburger.addEventListener('click', () => {
-    dropList.classList.toggle('show')
-})
+hamburger.addEventListener("click", () => {
+    dropList.classList.toggle("show");
+});
 
 //gestione modal eliminazione
 const modalButtons = document.querySelectorAll('[data-bs-toggle="modal"]');
-const confirmationButton = document.querySelector('#confirmationButton');
+const confirmationButton = document.querySelector("#confirmationButton");
 
-for(let modalButton of modalButtons) {
+for (let modalButton of modalButtons) {
+    modalButton.addEventListener("click", (e) => {
+        const action = e.target.getAttribute("data-action");
 
-    modalButton.addEventListener('click', e => {
-
-        const action = e.target.getAttribute('data-action');
-
-        confirmationButton.setAttribute('wire:click', action);
-
+        confirmationButton.setAttribute("wire:click", action);
     });
-
-};
-// OBSERVER
-const observer =new IntersectionObserver((entries)=>{
-    entries.forEach((entry)=>{
-        if (entry.isIntersecting){
-            entry.target.classList.add('show')
-        }else{
-            entry.target.classList.remove('show')
+}
+// fade in
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        } else {
+            entry.target.classList.remove("show");
         }
-    })
-})
+    });
+});
 
-let hiddenElements=document.querySelectorAll('.hidden');
-hiddenElements.forEach((el)=>{
-    observer.observe(el)
-})
+let hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => {
+    observer.observe(el);
+});
---
+++ resources/views/announcements/home.blade.php
@@ -0,0 +1,110 @@
+<x-main>
+    
+    <x-slot:title>Homepage</x-slot:title>
+
+    <section class="cards-section">
+        <div class="container pb-3">
+            <div class="row">
+                <h2 class="ann-title my-5">{{__('ui.exploreCategories')}}</h2>
+
+                <div class="col-6 col-md-2 my-2 offset-md-1">
+                    <a href="{{ route('announcements', 1) }}">
+                        <div class="category-card shadow hidden" style="--bcg:#b06680; --i:1;">
+                            <i class="fa-solid fa-car-side"></i> <span>{{__('ui.cars')}}</span>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-6 col-md-2 my-2">
+                    <a href="{{ route('announcements', 1) }}">
+                        <div class="category-card shadow hidden" style="--bcg:#FF971A;--i:2;">
+                            <i class="fa-solid fa-motorcycle"></i> <span>{{__('ui.motorcycle')}}</span>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-6 col-md-2 my-2">
+                    <a href="{{ route('announcements', 1) }}">
+                        <div class="category-card shadow hidden" style="--bcg:#97ECF1;--i:3;">
+                            <i class="fa-solid fa-ship"></i> <span>{{__('ui.boating')}}</span>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-6 col-md-2 my-2 ">
+                    <a href="{{ route('announcements', 1) }}">
+                        <div class="category-card shadow hidden" style="--bcg:#77DD77; --i:4;">
+                            <i class="fa-solid fa-gamepad"></i> <span>{{__('ui.video_games')}}</span>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-6 col-md-2 my-2">
+                    <a href="{{ route('announcements', 1) }}">
+                        <div class="category-card shadow hidden" style="--bcg:#fb6f92;--i:5;">
+                            <i class="fa-solid fa-mobile"></i> <span>{{__('ui.phone')}}</span>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-6 col-md-2 my-2 offset-md-1">
+                    <a href="{{ route('announcements', 1) }}">
+                        <div class="category-card shadow hidden" style="--bcg:#61f4de;--i:6;">
+                            <i class="fa-solid fa-laptop"></i> <span>Computer</span>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-6 col-md-2 my-2">
+                    <a href="{{ route('announcements', 1) }}">
+                        <div class="category-card shadow hidden" style="--bcg:#949494;--i:7;">
+                            <i class="fa-solid fa-coins"></i> <span>{{__('ui.collecting')}}</span>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-6 col-md-2 my-2">
+                    <a href="{{ route('announcements', 1) }}">
+                        <div class="category-card shadow hidden" style="--bcg:#FF6961;--i:8;">
+                            <i class="fa-solid fa-music"></i> <span>{{__('ui.music')}}</span>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-6 col-md-2 my-2">
+                    <a href="{{ route('announcements', 1) }}">
+                        <div class="category-card shadow hidden" style="--bcg:#b19CD9;--i:9;">
+                            <i class="fa-solid fa-film"></i> <span>{{__('ui.film')}}</span>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-6 col-md-2 my-2">
+                    <a href="{{ route('announcements', 1) }}">
+                        <div class="category-card shadow hidden" style="--bcg:#ffcc00;--i:10;">
+                            <i class="fa-solid fa-book"></i> <span>{{__('ui.books')}}</span>
+                        </div>
+                    </a>
+                </div>
+            </div>
+
+            <h2 class="ann-title my-5">{{__('ui.lastAnn')}}</h2>
+            <livewire:cards :where="$home" />
+
+        </div>
+    </section>
+
+    <div class="homebtn-container container-fluid position-relative">
+        <div class="row h-100">
+            <div class="col-12 col-md-6 sell_box">
+                <span class="hidden">{{__('ui.startSelling1')}}</span>
+                <span class="hidden">{{__('ui.startSelling2')}}</span>
+                <a href="{{route('announcements.create')}}" class="btn-home floatingItem">{{__('ui.createAnn')}}</a>
+            </div>
+            <div class="col-12 col-md-6 position-relative">
+                <img src="/storage/imgs/sell_final.png" alt="" class="position-absolute cards_img">
+            </div>
+        </div>
+    </div>
+
+</x-main>
